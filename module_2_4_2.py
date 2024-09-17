b7941577ebc33cc30274f80cd7917e64c2513376
Bez-B <79119302066@yandex.ru>
2024-09-17 21:08:23 +0300

    Update module_2_4_2.py

--- module_2_4_2.py
+++ module_2_4_2.py
@@ -5,13 +5,14 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 
-for i in range(1, len(numbers)):
-    for j in range(2, int(numbers[i] ** 0.5)+1):
+for num in numbers:
+    if num > 1:
         is_prime = True
-        if numbers[i] % numbers[j] == 0:
-            is_prime = False
-            not_primes.append(numbers[i])
-            break
+        for i in range(1, len(numbers)):
+            for j in range(2, int(numbers[i] ** 0.5)+1):
+                if numbers[i] % numbers[j] == 0:
+                    is_prime = False          
+                    break
         if is_prime:
             primes.append(numbers[i])
         else:
